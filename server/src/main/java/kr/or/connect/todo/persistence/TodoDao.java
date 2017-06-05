01bcd7d5ee21cc11106d937bc61eddb60ce4566a
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-06-05 23:20:26 +0900

    요구사항에 맞게 수정

--- server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
@@ -22,28 +22,6 @@ public class TodoDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
 	private RowMapper<Todo> rowMapper = BeanPropertyRowMapper.newInstance(Todo.class);
-
-	
-	// Connection 과 매핑을 할때 사용함
-
-	private static final String SELECT_BY_ID =
-			"SELECT id, todo, completed, date FROM TODO where id = :id";
-	private static final String DELETE_BY_ID = "DELETE FROM TODO WHERE id= :id";
-	private static final String DELETE_BY_Completed = "DELETE FROM TODO WHERE completed = 1";
-	private static final String COUNT_Todo = "SELECT COUNT(*) FROM Todo";
-	private static final String UPDATE =
-			"UPDATE Todo SET\n"
-			+ "completed = :completed \n"
-			+ "WHERE id = :id";
-	
-	private static final String SELECT_ALL =
-			"SELECT id, todo, completed, date  FROM Todo";
-
-	private static final String SELECT_Completed =
-			"SELECT id, todo, completed, date  FROM Todo where completed = 1";
-
-	private static final String SELECT_Uncompleted =
-			"SELECT id, todo, completed, date  FROM Todo where completed = 0";
 	
 	public TodoDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
@@ -52,63 +30,49 @@ public class TodoDao {
 				.usingGeneratedKeyColumns("id")
 				.usingColumns("todo","completed","date");
 	}
-	
-
 
 	public List<Todo> selectAll() {
 		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.query(SELECT_ALL, params, rowMapper);
+		return jdbc.query(TodoSqls.SELECT_ALL, params, rowMapper);
 	}
 	
 	public List<Todo> selectCompleted() {
 		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.query(SELECT_Completed, params, rowMapper);
+		return jdbc.query(TodoSqls.SELECT_Completed, params, rowMapper);
 	}
 	
 	public List<Todo> selectUncompleted() {
 		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.query(SELECT_Uncompleted, params, rowMapper);
+		return jdbc.query(TodoSqls.SELECT_Uncompleted, params, rowMapper);
 	}
 	
-
-	
-
 	// key value를 반환한다. 
 	public Integer insert(Todo todo) {
 		SqlParameterSource params = new BeanPropertySqlParameterSource(todo);
 		return insertAction.executeAndReturnKey(params).intValue();
 	}
 	
-	
-	public int countTodos() {
-		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.queryForObject(COUNT_Todo, params, Integer.class);
-	}
-	
 	public Todo selectById(Integer id) {
 		Map<String, Object> params = new HashMap<>();
 		params.put("id", id);
-		return jdbc.queryForObject(SELECT_BY_ID, params, rowMapper);
+		return jdbc.queryForObject(TodoSqls.SELECT_BY_ID, params, rowMapper);
 	}
 	
-
 	public int deleteById(Integer id) {
 			Map<String, ?> params = Collections.singletonMap("id", id);
-			return jdbc.update(DELETE_BY_ID, params);
+			return jdbc.update(TodoSqls.DELETE_BY_ID, params);
 	}
 	
 	// 완료 목록을 삭제하는 부분
 	public int deleteCompletedAll() {
 		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.update(DELETE_BY_Completed, params);
+		return jdbc.update(TodoSqls.DELETE_BY_Completed, params);
 	}
-	
 	
 	// complete 상태를 바꾸는 update 
 	public int update(Todo todo) {
 		SqlParameterSource params = new BeanPropertySqlParameterSource(todo);
-		return jdbc.update(UPDATE, params);
+		return jdbc.update(TodoSqls.UPDATE, params);
 	}
-	
 
 }
--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -1,6 +1,11 @@
 package kr.or.connect.todo.persistence;
 
 public class TodoSqls {
-	static final String DELETE_BY_ID =
-			"DELETE FROM todo WHERE id= :id";
+	static final String SELECT_BY_ID = "SELECT id, todo, completed, date FROM TODO where id = :id";
+	static final String SELECT_ALL = "SELECT id, todo, completed, date  FROM Todo";
+	static final String SELECT_Completed = "SELECT id, todo, completed, date  FROM Todo where completed = 1";
+	static final String SELECT_Uncompleted = "SELECT id, todo, completed, date  FROM Todo where completed = 0";
+	static final String DELETE_BY_ID = "DELETE FROM TODO WHERE id= :id";
+	static final String DELETE_BY_Completed = "DELETE FROM TODO WHERE completed = 1";
+	static final String UPDATE = "UPDATE Todo SET\n" + "completed = :completed \n" + "WHERE id = :id";
 }
